14197a555582daaba27e3c04b3cceab49580ad67
jamespericles <63256479+jamespericles@users.noreply.github.com>
2020-07-26 15:17:31 -0400

    annotated code

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
 const express = require("express");
 const path = require("path");
 
+// Set up Express app to listen on port 3000
 let app = express();
 let PORT = 3000;
 
+// Set up Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// Empty Array for notes
 let notes = [];
 
+// Routes
 app.get("*", function (req, res) {
   res.sendFile(path.join(__dirname, "index.html"));
 });
